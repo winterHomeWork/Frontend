dfab0a627345402cc9d6ce7c07e82372fd385463
최성훈 <chltjdgns1009@gmail.com>
2024-02-06 07:25:38 +0900

    로그인정보 폴더분리

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,12 @@ import Login from "./pages/Login";
 import Loading from "./pages/Loading";
 import Search from "pages/Search";
 import Main from "pages/main";
-import axios from "axios";
 import Filter from "pages/Filter";
-import customAxios from "lib/customAxios";
+import { GetLogin } from "lib/getLogin";
 
 function App() {
-  const code = new URLSearchParams(window.location.search).get("code");
-  useEffect(() => {
-    if (code) {
-      const kakaoGet = async () => {
-        const baseUrl = "https://prod-server.xquare.app/nudia/kakao";
-        try {
-          const response = await axios.post(baseUrl, {
-            headers: {
-              "Content-Type": "application/json",
-            },
-            code,
-          });
-          if (response.data.access_token && response.data.refresh_token) {
-            localStorage.setItem("access", response.data.access_token);
-            localStorage.setItem("refresh", response.data.refresh_token);
-          }
-          if (localStorage.getItem("access") && localStorage.getItem("refresh")) {
-            const { data } = await customAxios.get("/user");
-            localStorage.setItem("name", data.nickname)
-            localStorage.setItem("email",data.email)
-            window.location.replace("/main")
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      kakaoGet();
-    }
-  }, [code]);
+
+  GetLogin();
 
   return (
     <Router>
