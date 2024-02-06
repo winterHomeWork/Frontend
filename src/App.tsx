fbc27d78f04af1a6ac61c2470f0a8b7e47e8273f
최성훈 <chltjdgns1009@gmail.com>
2024-02-06 23:02:33 +0900

    카카오 로그인 인가코드발급

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ import Login from "./pages/Login";
 import Loading from "./pages/Loading";
 
 function App() {
+  const code = new URLSearchParams(window.location.search).get("code",);
+  if (code) {
+    console.log("인가코드", code)
+    localStorage.setItem("code", code)
+    window.location.replace("/main")
+  }
+  
   return (
     <Router>
       <Routes>
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -4,12 +4,21 @@ import whiteLogo from 'assets/WhiteLogo.svg'
 import talk from 'assets/talk.svg'
 
 const Login = () => {
+
+    const Rest_api_key = "9518b1b3124468b73a6625b1c62623c0"
+    const redirect_uri = "http://localhost:3000/kakao/callback"
+    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
+
+    const handleLogin = ()=>{
+      window.location.href = kakaoURL
+  }
+
   return (
     <S.Display>
       <S.Logo src={whiteLogo} />
       <S.BottomBox>
         <S.Title>Login</S.Title>
-        <S.KakaoLayout>
+        <S.KakaoLayout onClick={handleLogin}>
           <S.Talk src={talk} />
           카카오 로그인
         </S.KakaoLayout>
