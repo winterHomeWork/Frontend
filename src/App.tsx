cfa4ede86a7ba6ec7e947cda8902f760fc44fa8c
최성훈 <chltjdgns1009@gmail.com>
2024-02-06 19:46:27 +0900

    폴더구조랑 라우터

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
