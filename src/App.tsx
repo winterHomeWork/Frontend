16c20a9497a80a4c0cda70947cfdcfa983a0c41d
최성훈 <chltjdgns1009@gmail.com>
2024-02-07 00:10:44 +0900

    백엔드에 인가코드 전송

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,22 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Loading from "./pages/Loading";
+import Imsi from "pages/imsi";
 
 function App() {
   const code = new URLSearchParams(window.location.search).get("code",);
   if (code) {
     console.log("인가코드", code)
     localStorage.setItem("code", code)
-    window.location.replace("/main")
+    window.location.replace("/imsi")
   }
-  
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Loading />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/imsi" element={<Imsi />} />
       </Routes>
     </Router>
   );
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import * as S from 'pages/Login/style'
-import whiteLogo from 'assets/WhiteLogo.svg'
-import talk from 'assets/talk.svg'
+import React, { useEffect } from "react";
+import * as S from "pages/Login/style";
+import whiteLogo from "assets/WhiteLogo.svg";
+import talk from "assets/talk.svg";
+import axios from "axios";
 
 const Login = () => {
+  const Rest_api_key = "9518b1b3124468b73a6625b1c62623c0";
+  const redirect_uri = "http://localhost:3000/kakao/callback";
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
-    const Rest_api_key = "9518b1b3124468b73a6625b1c62623c0"
-    const redirect_uri = "http://localhost:3000/kakao/callback"
-    const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-
-    const handleLogin = ()=>{
-      window.location.href = kakaoURL
-  }
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
 
   return (
     <S.Display>
