af02f1ab591cdb00d88a1ca4ce5f3957d9e00013
최성훈 <chltjdgns1009@gmail.com>
2024-02-07 11:02:28 +0900

    search input

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import * as S from 'pages/Search/style'
+import backToMenu from 'assets/BackToMenu.svg'
 
 const Search = () => {
   return (
     <S.Display>
       <S.TopBox>
-
+        <S.InputLayout>
+          <S.BackToMenu src={backToMenu} />
+          <S.Input />
+        </S.InputLayout>
       </S.TopBox>
+      <S.MidGap>
+
+      </S.MidGap>
       <S.BottomBox>
-        
+
       </S.BottomBox>
     </S.Display>
   );
--- src/pages/Search/style.ts
+++ src/pages/Search/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Search from "assets/Search.svg"
 
 export const Display = styled.div`
   width: 100%;
@@ -9,11 +10,39 @@ export const TopBox = styled.div`
   width: 100%;
   height: 21%;
   background-color: #ffffff;
+  padding: 28px 26px 28px 18px;
+`
+export const MidGap = styled.div`
+  width: 100%;
+  height: 20px;
+  background-color: #E3E3E3;
 `
 export const BottomBox = styled.div`
-  position: absolute;
   width: 100%;
-  height: 76%;
-  bottom: 0;
+  height: 100%;
   background-color: #ffffff;
+`
+export const InputLayout = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`
+export const BackToMenu = styled.img`
+  width: 12px;
+  height: 18px;
+  stroke-width: 2px;
+`
+export const Input = styled.input`
+  width: 100%;
+  height: 40px;
+  border-radius: 8px;
+  background: #E3E3E3;
+  border: none;
+  background-image: url(${Search});
+  background-repeat: no-repeat;
+  background-size: 20px 23.182px;
+  background-position-x: 96%;
+  background-position-y: center;
 `
