2a6df159a25523149cb021fa03ec8e0e8f41e9d8
youngeunkim <pkhj286@gmail.com>
2024-02-08 22:46:32 +0900

    Add : filter 페이지

--- src/pages/Filter/index.tsx
+++ src/pages/Filter/index.tsx
@@ -13,30 +13,38 @@ const Filter = () => {
       </S.FilterLayout>
       <S.Filter>
         <S.FilterBox>
-          <S.PopularInput type="radio" />
+          <S.PopularInput type="radio" name="popular" />
           <S.Text>인기순</S.Text>
         </S.FilterBox>
         <S.FilterBox>
-          <S.CheapInput type="radio" />
+          <S.CheapInput type="radio" name="cheap" />
           <S.Text>저가순</S.Text>
         </S.FilterBox>
         <S.FilterBox>
-          <S.ExpenInput type="radio" />
+          <S.ExpenInput type="radio" name="expen" />
           <S.Text>고가순</S.Text>
         </S.FilterBox>
         <S.FilterBox>
-          <S.TanInput type="radio" />
+          <S.TanInput type="radio" name="tan" />
           <S.Text>탄수화물 함유량 순</S.Text>
         </S.FilterBox>
         <S.FilterBox>
-          <S.JibangInput type="radio" />
+          <S.JibangInput type="radio" name="jibang" />
           <S.Text>지방 함유량 순</S.Text>
         </S.FilterBox>
         <S.FilterBox>
-          <S.SugarInput type="radio" />
+          <S.SugarInput type="radio" name="sugar" />
           <S.Text>당류 낮은 순</S.Text>
         </S.FilterBox>
       </S.Filter>
+      <S.Calorie>
+        <S.CalorieText>칼로리</S.CalorieText>
+        <S.CalorieBox>
+          <S.CalorieInput type="number" placeholder="kcal" />-
+          <S.CalorieInput type="number" placeholder="kcal" />
+        </S.CalorieBox>
+        <S.CalorieGraph></S.CalorieGraph>
+      </S.Calorie>
     </>
   );
 };
--- src/pages/Filter/style.ts
+++ src/pages/Filter/style.ts
@@ -5,7 +5,7 @@ export const FilterLayout = styled.div`
   align-items: center;
   justify-content: center;
   border-bottom: solid;
-  margin-bottom: 8%;
+  margin-bottom: 5%;
 `;
 
 export const FilterHeader = styled.div`
@@ -39,7 +39,8 @@ export const FilterBox = styled.label`
   display: flex;
   align-items: center;
   width: 100%;
-  margin-bottom: 12%;
+  margin-bottom: 10%;
+  border-bottom: 1px #e9e9e9 solid;
 `;
 
 export const Text = styled.p`
@@ -47,37 +48,76 @@ export const Text = styled.p`
   justify-content: center;
   color: #000;
   font-family: Pretendard;
-  font-size: 24px;
+  font-size: 20px;
   font-weight: 400;
-  margin-left: 2%;
+  margin: 0 0 5% 2%;
 `;
 
 export const PopularInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
 
 export const CheapInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
 
 export const ExpenInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
 
 export const TanInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
 
 export const JibangInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
 
 export const SugarInput = styled.input`
-  width: 2.5em;
-  height: 2.5em;
+  width: 2em;
+  height: 2em;
+  margin-bottom: 5%;
 `;
+
+export const Calorie = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CalorieText = styled.p`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 24px;
+  font-weight: 400;
+  padding-left: 5%;
+`;
+
+export const CalorieBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+`;
+
+export const CalorieInput = styled.input`
+  display: flex;
+  align-items: center;
+  border-radius: 7px;
+  border: 1px solid #000;
+  width: 10em;
+  height: 3em;
+  margin: 4%;
+  padding-left: 12.5%;
+`;
+
+export const CalorieGraph = styled.div``;
